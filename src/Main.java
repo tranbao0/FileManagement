9fbcf629cdae432feaad22410c67301561a877dd
tranbao0 <hail246005@gmail.com>
2025-12-07 17:19:55 -0800

    UUUGHGHGHGHGH

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        // launch the gui
+        Persistence.DemoDataInitializer.initialize(new Management.FileSystem());
+
+        // launch GUI
         GUI.MainWindow.main(args);
     }
 }
